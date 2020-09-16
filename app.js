c61767acf2a7b664dcfcd9e6f94934ff28be5ca8
CarolLiu <carol.gugu@hotmail.com>
2020-09-16 12:01:07 +1000

    print entire array, particular element & extra notes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
 
-const greenMsg = chalk.red.bold('Error!')
+console.log(process.argv)
 
-
-console.log(chalk.green('success!'))
-console.log(greenMsg)
-
-console.log(process.argv[2])
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
