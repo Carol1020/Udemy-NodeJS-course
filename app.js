f9dd0b2a80f5e74b9151b3545a9cc35b158fb04a
CarolLiu <carol.gugu@hotmail.com>
2020-09-21 11:04:01 +1000

    create list command

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ yargs.command({
     command: 'list',
     describe: 'List a note',
     handler() {
-        console.log('Listing the note')
+        notes.listNotes()
     }
 })
 
--- notes.js
+++ notes.js
@@ -50,6 +50,17 @@ const removeNote = (title) => {
     saveNotes(notesToKeep);
 }
 
+
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.inverse('Your notes'))
+
+    notes.forEach((note) => {
+        console.log(note.title)
+    });
+}
+
+
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
@@ -66,8 +77,10 @@ const loadNotes = () => {
 
 }
 
+
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes
 }
